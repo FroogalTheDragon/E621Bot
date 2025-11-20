489b9bc2050efef5a3bd5b111c16f4b770592137
codecanna <codecannamw@gmail.com>
2025-11-20 02:55:36 -0700

    added pools urls

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,24 +1,69 @@
-export const baseUrl: string = "https://e621.net/posts";
-export const baseUrlTags: string = baseUrl.concat(".json"); // Base e621 url
+/**
+ * Base e621.net URL
+ */
+export const baseUrl: string = "https://e621.net";
+
+/**
+ * Different endpoints on e621
+ */
+export const endpoint = {
+  posts: "/posts.json",
+  pools: "/pools.json",
+};
+
+/**
+ * Pool search types
+ */
+export const poolSearch = {
+  nameMatches: "name_matches",
+  id: "id",
+  descriptionMatches: "description_matches",
+  creatorName: "creator_name",
+  creatorId: "creator_id",
+  isActive: "is_active",
+  category: "category",
+  order: {
+    name: "name",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+    postCount: "post_count",
+  },
+};
+
+/**
+ * Post ratings
+ */
 export const rating = { // The rating of a post or a rating to search by
-    safe: "rating:s",
-    questionable: "rating:q",
-    explicit: "rating:e"
+  safe: "rating:s",
+  questionable: "rating:q",
+  explicit: "rating:e",
 };
+
+/**
+ * Post filetypes
+ */
 export const fileType = {
-    jpg: "type:jpg",
-    png: "type:png",
-    gif: "type:gif",
-    mp4: "type:mp4"
+  jpg: "type:jpg",
+  png: "type:png",
+  gif: "type:gif",
+  mp4: "type:mp4",
 };
+
+/**
+ * Orders to load posts in
+ */
 export const order = {
-    score: "order:score",
-    favcount: "order:favcount",
-    random: "order:random",
-    hot: "order%3Ahot"
-}
+  score: "order:score",
+  favcount: "order:favcount",
+  random: "order:random",
+  hot: "order%3Ahot",
+};
+
+/**
+ * Date parameters to get posts by
+ */
 export const date = {
-    today: "date:today",
-    yesterday: "date:yesterday",
-    byDate: "date:"
-}
+  today: "date:today",
+  yesterday: "date:yesterday",
+  byDate: "date:",
+};
