ac24bad799e8030ed89e598288160c99bb8488cf
codecanna <codecannamw@gmail.com>
2026-01-01 00:06:59 -0700

    changed createBlacklistDb to createUserDb

--- db/utils/createDb.ts
+++ db/utils/createDb.ts
@@ -1,9 +1,9 @@
 import { PathLike } from "node:fs";
-import { createBlacklistDb } from "../migration.ts";
+import { createUserDb } from "../migration.ts";
 
 export function createDatabase(dbFile: PathLike): boolean {
   try {
-    createBlacklistDb(dbFile);
+    createUserDb(dbFile);
     return true;
   } catch (err) {
     console.error(`Failed to create Database: ${dbFile}: ${err}`);
