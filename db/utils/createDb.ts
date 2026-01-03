d692660ec7a963ed3676bc08fa293b9a931d47e9
codecanna <codecannamw@gmail.com>
2026-01-02 18:40:13 -0700

    using try catch for this function now

--- db/utils/createDb.ts
+++ db/utils/createDb.ts
@@ -1,12 +1,12 @@
 import { PathLike } from "node:fs";
 import { createUserDb } from "../migration.ts";
+import { E621DatabaseError } from "../../types/Error.ts";
 
-export function createDatabase(dbFile: PathLike): boolean {
+export function createDatabase(dbFile: PathLike) {
   try {
     createUserDb(dbFile);
-    return true;
   } catch (err) {
     console.error(`Failed to create Database: ${dbFile}: ${err}`);
-    return false;
+    throw new E621DatabaseError(`Failed to create Database: ${dbFile}: ${err}`);
   }
 }
